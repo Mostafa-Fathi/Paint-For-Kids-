ac9b1a8a525a4db3e199ff1ee4565f1ecf538ecc
Asmaa-Zaki <asmaazaki886@gmail.com>
2022-01-31 02:47:56 +0200

    edit

--- CMUgraphicsLib/colors.h
+++ CMUgraphicsLib/colors.h
@@ -2,7 +2,6 @@
 See "version.h" for version info and copyright information
 This file was last modified on 05.16.1999
 */
-#include <string.h>
 #ifndef COLORS_H
 #define COLORS_H
 
@@ -25,34 +24,6 @@ class color {
     unsigned char ucBlue;
 
     // -------------remember add all used colors-----------------
-    color getCurrentColor(string Color)
-    {
-        const char* nColor = Color.c_str();
-        if (strcmp(nColor, "RED") == 0)
-        {
-            return color(255, 0, 0);
-        }
-        else if (strcmp(nColor, "BLUE") == 0)
-        {
-            return color(0, 0, 255);
-        }
-        else if (strcmp(nColor, "BLACK") == 0)
-        {
-            return color(0, 0, 0);
-        }
-        else if (strcmp(nColor, "GREEN") == 0)
-        {
-            return color(0, 255, 0);
-        }
-        else if (strcmp(nColor, "YELLOW") == 0)
-        {
-            return color(255, 255, 0);
-        }
-        else if (strcmp(nColor, "BROWN") == 0)
-        {
-            return color(165, 42, 42);;
-        }
-    }
 
 };
 
